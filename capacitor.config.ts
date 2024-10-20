367086bce8194bcd7d91030cc40d12b7c8e1d47e
Marcelo Sampaio <marcelosampaiocontato@gmail.com>
2024-10-20 08:40:26 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'connect_stu',
+  webDir: 'www'
+};
+
+export default config;
