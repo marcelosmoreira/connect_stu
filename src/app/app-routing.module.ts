b27bf6600c97a4650bff0f82e74317404e9855b0
Marcelo Sampaio <marcelosampaiocontato@gmail.com>
2024-10-20 18:57:19 -0300

    Modelando a tela de Carregamento Inicial e a Tela de Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: '',
-    redirectTo: 'screen-loading',  // Redireciona primeiro para a tela de loading
+    redirectTo: 'screen-loading',
     pathMatch: 'full'
   },
   {
