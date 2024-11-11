fd8b90d6eeb6944b80f8cedc16d0073086c860ee
Yan Gabriel <yan@localhost.localdomain>
2024-11-10 23:14:20 -0300

    Adição do modal de criação de post na page /home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
+  @ViewChild(IonModal) modal: IonModal | undefined;
+  message: string = "";
+  name: string = "";
 
   constructor() { }
 
-  ngOnInit() {
-    console.log("teste");
+  ngOnInit() { }
 
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
+
+  cancel() {
+    this.modal?.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal?.dismiss(this.name, 'confirm');
   }
 
 }
